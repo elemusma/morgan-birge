71b3113370df1b45c7cf007b33b2d95a4d0df4a0
Efrain Lemus <efraintlemusm@gmail.com>
2022-08-06 20:51:55 -0400

    after updating app url on webhook

--- front-page.php
+++ front-page.php
@@ -4,7 +4,8 @@ echo '<div class="position-relative z-1">';
 echo '<p>hello again! after doing automatic deploy added SSH url to webhook.</p>';
 echo '<p>after uncommenting line of code and updating webhook url. After updating the server ID</p>';
 echo '<p>after updating the url.</p>';
-echo '<p>after updating main gitautodeploy.php in account rather than app</p>';
+echo '<p>after updating main gitautodeploy.php in account rather than app.</p>';
+echo '<p>after after updating app url on webhook</p>';
 echo '</div>';
 
 // start of intro
