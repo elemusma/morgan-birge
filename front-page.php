5c49acea149b27d0812ca52e0aa662b6e58e8dd2
Efrain Lemus <efraintlemusm@gmail.com>
2022-08-06 20:21:05 -0400

    after updating server ID

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 echo '<div class="position-relative z-1">';
 echo '<p>hello again! after doing automatic deploy added SSH url to webhook.</p>';
-echo '<p>after uncommenting line of code and updating webhook url</p>';
+echo '<p>after uncommenting line of code and updating webhook url. After updating the server ID</p>';
 echo '</div>';
 
 // start of intro
